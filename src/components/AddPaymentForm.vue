7319c4a4d3581cfd27802e8963bdcbb47d217413
Symegu <89914120+Symegu@users.noreply.github.com>
2022-04-25 10:40:25 +0300

    2часть урока

---
+++ src/components/AddPaymentForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="form-wrapper">
+      <input v-model="date" placeholder="date" />
+      <input v-model="category" placeholder="category" />
+      <input v-model="value" placeholder="value" />
+      <br>
+      <button @click="onClickSave">Save</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddPaymentForm',
+  data () {
+    return {
+      date: '',
+      category: '',
+      value: ''
+    }
+  },
+  computed: {
+    getCurrentDate () {
+      const today = new Date()
+      const d = today.getDate()
+      const m = today.getMonth() + 1
+      const y = today.getFullYear()
+      return `${d}.${m}.${y}`
+    }
+  },
+  methods: {
+    onClickSave () {
+      const data = {
+        date: this.date || this.getCurrentDate,
+        category: this.category,
+        value: this.value
+      }
+      this.$emit('addNewPayment', data)
+      console.log(data)
+    }
+  }
+}
+</script>
